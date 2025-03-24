bce06d06b9cd3072858df4c8f1a693f2107922ab
Shahbaz Mehmood <116480008+samishah11@users.noreply.github.com>
2025-03-24 11:33:53 +0500

    snake.py

--- snake.py
+++ snake.py
@@ -1,4 +1,3 @@
-import pyodide
 import random
 import js
 
@@ -15,7 +14,7 @@ food = [random.randint(0, WIDTH // BLOCK_SIZE - 1) * BLOCK_SIZE,
 def update():
     global direction, snake, food
     head = snake[-1][:]
-    
+
     if direction == "LEFT":
         head[0] -= BLOCK_SIZE
     elif direction == "RIGHT":
@@ -24,22 +23,27 @@ def update():
         head[1] -= BLOCK_SIZE
     elif direction == "DOWN":
         head[1] += BLOCK_SIZE
-    
+
     # Wrap-around logic
     head[0] %= WIDTH
     head[1] %= HEIGHT
-    
+
     snake.append(head)
     if head == food:
         food = [random.randint(0, WIDTH // BLOCK_SIZE - 1) * BLOCK_SIZE,
                 random.randint(0, HEIGHT // BLOCK_SIZE - 1) * BLOCK_SIZE]
     else:
         snake.pop(0)
-    
+
     # Send updated game state to JavaScript
-    js.update_canvas(snake, food)
+    js.update_canvas(to_js(snake), to_js(food))
 
 def set_direction(new_direction):
     global direction
     if (direction, new_direction) not in [("LEFT", "RIGHT"), ("RIGHT", "LEFT"), ("UP", "DOWN"), ("DOWN", "UP")]:
         direction = new_direction
+
+# Expose functions to JavaScript
+from pyodide.ffi import to_js
+js.set("update", update)
+js.set("set_direction", set_direction)
